1a59baaf2ff57ce006dea325117d952dc63c24d7
NahomTM <88828909+NahomTM@users.noreply.github.com>
2021-08-31 00:04:09 +0300

    Add files via upload

--- User registeration.cpp
+++ User registeration.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 #define capacity 100
+
+
 struct User
 {
     string fName;
@@ -9,83 +11,70 @@ struct User
     string username;
     string password;
 };
-struct addBook
-{
-    string Title;
-    string bookNo;
-    string Author;
-    unsigned int YOP;
-};
-char option;
+char option, choice;
 int n = 0, m = 0;
-string get_user(User);
-string User_reg(User);
+string get_user();
+string get_user_email();
+string User_reg();
 //void add_book();
-void fill_user(User);
+void fill_user();
+User usersInfoArray[capacity];
 
 int main()
 {
-    User U[n];
-    fill_user(U[n]);
+    fill_user();
+
     cout<<"\n   \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd"<<endl;
     cout << "\t    WELCOME TO OUR LIBRARY \n";
     cout<<"   \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n"<<endl;
     cout<<"   \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd MENU OPTIONS \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n"<<endl;
     cout <<"\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd \n";
-
     cout <<"\t User Registeration              ------ [1] \n";
-    cout <<"\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd \n";
-
-    cout <<"\t Add new book                    ------ [2] \n ";
     cout <<"\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd \n";
     cin>>option;
     if (option == '1')
     {
-        User_reg(U[n]);
-    }
-    else if (option == '2')
-    {
-        return 0;
+        User_reg();
     }
 }
-void fill_user(User U[])
+void fill_user()
 {
     for(int i=0; i<capacity; i++)
     {
-        U[capacity].username = "Somebody";
+        usersInfoArray[i] = {};
+        usersInfoArray[i].username = "Somebody";
     }
 }
-string User_reg(User U[])
+string User_reg()
 {
-
+ comback:
     cout<<"Please fill the informations below to register in our library system."<<endl;
     cout<<"Enter you first name: ";
-    cin>>U[n].fName;
+    cin>>usersInfoArray[n].fName;
     cout<<"\nEnter your last name: ";
-    cin>>U[n].lName;
-    cout<<"\nEnter your email address: ";
-    cin>>U[n].email;
-    get_user(U[n]);
+    cin>>usersInfoArray[n].lName;
+    get_user_email();
+    get_user();
     cout<<"\nEnter your password.";
-    cin>>U[n].password;
+    cin>>usersInfoArray[n].password;
     cout<<"Thank you for your cooperation.";
     n++;
-
-}
-void add_book()
-{
-
-
-}
-string get_user(User U[])
-{
-comehere:
-    bool taken = false;
-    cout<<"\nEnter a user name: ";
-    cin>>U[n].username;
-    for(int i = 0; i < capacity; i++)
+    cout<<"\nPress 'a' to return to register another user or 'b' to terminate the program.";
+    cin>>choice;
+    if(choice == 'a')
     {
-        int Eq = (U[n].username).compare(U[i].username);
+        goto comback;
+    }
+}
+string get_user()
+{
+ comehere:
+    cout<<"\nEnter a user name: ";
+    cin>>usersInfoArray[n].username;
+    for(int i = 0; i < n; i++)
+    {
+        int Eq = (usersInfoArray[n].username).compare(usersInfoArray[i].username);
+
         if(Eq == 0)
         {
             cout << "This user name is taken";
@@ -93,6 +82,25 @@ comehere:
             goto comehere;
         }
     }
+    return usersInfoArray[n].username;
+}
+string get_user_email()
+{
+ comehere:
+    cout<<"\nEnter your email address: ";
+    cin>>usersInfoArray[n].email;
+    string str = usersInfoArray[n].email;
+    size_t foundAt = str.find("@");
+    if (foundAt == string::npos){
+        cout << "Your Email Should include @\n";
+        goto comehere;
+    }
+    size_t foundDot = str.find(".com");
+    if (foundDot == string::npos){
+        cout << "Your Email Should include .com\n";
+        goto comehere;
+    }
 
-    return U[n].username;
+
+    return usersInfoArray[n].email;
 }
